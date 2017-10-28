53bb9dbcc6ccb1738c9d2a512ed87beb2d49fcca
orion55 <grol55@mail.ru>
2017-10-28 23:06:51 +0500

    menu create

--- wp-content/themes/stoma/functions.php
+++ wp-content/themes/stoma/functions.php
@@ -44,7 +44,7 @@ if ( ! function_exists( 'stoma_setup' ) ) :
 
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
-			'menu-1' => esc_html__( 'Верхнее меню', 'stoma' ),
+			'menu-1'    => esc_html__( 'Верхнее меню', 'stoma' ),
 			'in_footer' => 'Меню в подвале'
 		) );
 
@@ -82,7 +82,7 @@ if ( ! function_exists( 'stoma_setup' ) ) :
 		) );
 
 		add_filter( 'show_admin_bar', '__return_false' );
-		remove_action('welcome_panel', 'wp_welcome_panel');
+		remove_action( 'welcome_panel', 'wp_welcome_panel' );
 	}
 endif;
 add_action( 'after_setup_theme', 'stoma_setup' );
@@ -124,6 +124,8 @@ add_action( 'widgets_init', 'stoma_widgets_init' );
  */
 function stoma_scripts() {
 	wp_enqueue_style( 'stoma-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'stoma-style-main', get_template_directory_uri() . '/assets/css/main.min.css' );
+	wp_enqueue_style( 'stoma-style-vendor', get_template_directory_uri() . '/assets/css/vendor.min.css' );
 
 	wp_enqueue_script( 'stoma-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
@@ -132,6 +134,8 @@ function stoma_scripts() {
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
+	wp_enqueue_script('jquery');
+	wp_enqueue_script( 'stoma-main', get_template_directory_uri() . '/assets/js/main.min.js', array('jquery'), '20171028', true );
 }
 
 add_action( 'wp_enqueue_scripts', 'stoma_scripts' );
